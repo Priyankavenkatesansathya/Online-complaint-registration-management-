b5850a600d3afce71cab9509d0d9dc6336ef123b
BALAMURUGAN S <146547469+BALA2908@users.noreply.github.com>
2024-11-13 11:18:57 +0530

    Delete backend directory

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://sheikxm:sheik053123@sheikprojects.otin9.mongodb.net/?retryWrites=true&w=majority&appName=sheikprojects")
-.then(()=>{
-   console.log("connected to mongodb")
-})
